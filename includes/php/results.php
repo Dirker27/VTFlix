c4ae2d7fc6b6ef056d61a102216e53413127b26f
Dirk <hortensd@gmail.com>
2014-05-01 16:01:13 -0400

    Start of something not horrid

---
+++ includes/php/results.php
@@ -0,0 +1,13 @@
+<html>
+	
+	<head>
+	</head>
+
+	<body>
+		<?php
+			echo 'RELATION: ' . $_REQUEST['relation'] . "<br/>";
+			echo 'QUERY: ' . $_REQUEST['query'] . "<br/>";
+		?>
+	</body>
+
+<html>
