179eb09c7e31af40d9321c04832ec127e301cbb7
Dirk <hortensd@gmail.com>
2014-05-01 16:25:36 -0400

    php start of work

--- includes/php/results.php
+++ includes/php/results.php
@@ -1,13 +1,38 @@
+<?php
+	$r = $_REQUEST['relation'];
+	$q = $_REQUEST['query'];
+
+	//echo 'RELATION: ' . $r . "<br/>";
+	//echo 'QUERY: ' . $q . "<br/>";
+
+	$sql = ';';
+	switch($q) {
+		case 1:
+		$sql = 'SELECT COUNT(*) FROM Performer;';
+		break;
+
+		case 2:
+		$sql = 'SELECT COUNT(*) FROM Director;';
+		break;
+
+		default:
+		break;
+	}
+
+	$con = pg_connect("port=5432 dbname=debauchery user=Dirker27 password=sH@dow77") 
+		or die ("Failed to connect to DB: " . pg_last_error());
+	$result = pg_query($con, $sql) 
+		or die("Could not process query: " . pg_last_error());
+?>
+
 <html>
 	
 	<head>
+		<title> Results? </title>
 	</head>
 
 	<body>
-		<?php
-			echo 'RELATION: ' . $_REQUEST['relation'] . "<br/>";
-			echo 'QUERY: ' . $_REQUEST['query'] . "<br/>";
-		?>
+		<?php echo $result; ?>
 	</body>
 
 <html>
