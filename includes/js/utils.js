74ed85154164f470caf26a5df46a6f48ecbfed30
Dirk <hortensd@gmail.com>
2014-05-06 10:46:53 -0400

    trimming

--- includes/js/utils.js
+++ includes/js/utils.js
@@ -40,27 +40,27 @@ function sendQuery(q) {
 	switch(q) {
 		case 1:
 		// List 10 Brad Pitt Movies
-		sql = "SELECT title FROM VideoInfo WHERE vid IN (SELECT vid FROM ActIn WHERE pid = (SELECT pid FROM Performer WHERE first_name = 'Brad' AND last_name = 'Pitt')) ORDER BY release_year DESC LIMIT 10;";
+		sql = "SELECT title FROM VideoInfo WHERE vid IN (SELECT vid FROM ActIn WHERE pid = (SELECT pid FROM Performer WHERE first_name = 'Brad' AND last_name = 'Pitt')) ORDER BY release_year DESC LIMIT 10";
 		break;
 
 		case 2:
 		// User with most friends
-		sql = "SELECT first_name, last_name FROM UserInfo, (SELECT uid1 AS u, COUNT(*) AS c FROM Friends GROUP BY uid1 ORDER BY c DESC LIMIT 1) AS f WHERE f.u = uid;";
+		sql = "SELECT first_name, last_name FROM UserInfo, (SELECT uid1 AS u, COUNT(*) AS c FROM Friends GROUP BY uid1 ORDER BY c DESC LIMIT 1) AS f WHERE f.u = uid";
 		break;
 
 		case 3:
 		// Performer in most movies
-		sql = "SELECT first_name, last_name FROM Performer WHERE (SELECT pid, COUNT(*) AS c FROM ActIn GROUP BY pid ORDER BY c DESC LIMIT 1 WHERE vid IN (SELECT mid FROM MovieInfo));";
+		sql = "SELECT first_name, last_name FROM Performer WHERE (SELECT pid, COUNT(*) AS c FROM ActIn GROUP BY pid ORDER BY c DESC LIMIT 1 WHERE vid IN (SELECT mid FROM MovieInfo))";
 		break;
 
 		case 4:
 		// List 10 videos in the Fantasy genre
-		sql = "SELECT title FROM Videos WHERE (SELECT vid FROM BelongToGenre WHERE genre = 'Fantasy') DESC LIMIT 10;";
+		sql = "SELECT title FROM Videos WHERE (SELECT vid FROM BelongToGenre WHERE genre = 'Fantasy') DESC LIMIT 10";
 		break;
 
 		case 5:
 		// List all friends of John Abbey
-		sql = "SELECT first_name, last_name FROM UserInfo WHERE (SELECT uid FROM Friends WHERE uid1 = (SELECT uid FROM UserInfo WHERE first_name = 'John' AND last_name = 'Abbey');";
+		sql = "SELECT first_name, last_name FROM UserInfo WHERE (SELECT uid FROM Friends WHERE uid1 = (SELECT uid FROM UserInfo WHERE first_name = 'John' AND last_name = 'Abbey')";
 		break;
 
 		default:
