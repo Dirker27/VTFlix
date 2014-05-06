44bd4e32cd8e64ed49efc85a1386b1cabf8d64ff
Dirk <hortensd@gmail.com>
2014-05-06 14:46:44 -0400

    Documentation for Ariel's sake

--- includes/php/db_result.php
+++ includes/php/db_result.php
@@ -2,27 +2,40 @@
 
 function QueryDB($sql)
 {
+	//~ EXECUTE QUERY =========================================== ~//
+	//
+	// Establish connection
 	$con = pg_connect("port=5432 dbname=debauchery user=dirker27 password=sH@dow77") 
 		or die ("Failed to connect to DB: " . pg_last_error());
+	//
+	// Query database at connection
 	$result = pg_query($con, $sql) 
 		or die("Could not process query: " . pg_last_error());
 	
-	$i = 0;
-	$rows = array();	// vals
+	//~ DUMP RESULTS TO ARRAY =================================== ~//
+	///
+	// Initialize
+	$rows = array();		// rows
 	$rows[-1] = array();	// columns
+	///
+	//- Store by Rows ------------------------------------=
+	//
+	// Extract Rows
+	$i = 0;
 	while ($row = pg_fetch_row($result)) {
 		$rows[$i] = array();
-
+		//
+		// Extract Values
 		$j = 0;
 		foreach ($row as $value) {
 			$rows[$i][$j] = $value;
-
+			//
+			// Store Columnn Names on first pass
 			if ($i == 0) {
 				$rows[-1][$j] = pg_field_name($result, $j);
 			}
 			$j += 1;
 		}
-
 		$i += 1;
 	}
 
@@ -59,51 +72,49 @@ function GenerateTable($rows)
 	$html .= "\t</tbody>";
 
 	$html .= '</table>';
-
 	return $html;	
 }
 
 function GetResult()
 {
 	$ILLEGAL_COMMANDS = array(	'ALTER',
-							'CREATE',
-							'DROP',
-							'INSERT',
-							'MODIFY',
-							'TRUNCATE',
-							'UPDATE');
-	$q = $_REQUEST['query'];
-	
-	//~ EXTRACT INPUT TO SQL ================================ ~//
+								'CREATE',
+								'DROP',
+								'INSERT',
+								'MODIFY',
+								'TRUNCATE',
+								'UPDATE');
+	$sql = $_REQUEST['query'];
+
+	//~ QUERY SCRUBBING ========================================= ~//
+	///
+	//- If No Query Given --------------------------------=
 	//
-	//- Relation -------------------------------------=
-	//
-	$sql = '';
-	if ($q == '') {
+	if ($sql == '') {
 		return "<p>No Query Given.</p>";
 	}
-
-	// <Scrub query here somewhere>
-	$tok = strtok($q, " \n\t,;");
+	///
+	//- Scan For Illegal Keywords ------------------------=
+	//
+	// Tokenize (get first)
+	$tok = strtok($sql, " \n\t,;");
+	//
+	// While unscanned token
 	while ($tok) {
+		//
+		// If in Blacklist, return error.
 		$t = strtoupper($tok);
 		if (in_array($t, $ILLEGAL_COMMANDS)) {
 			return "<p><b>Aborted.</b><br/>Illegal Command Detected: $t</p>";
 		}
+		//
+		// Get next token
 		$tok = strtok(" \n\t,;");
 	}
-	$sql = $q;
 
+	//~ EXECUTE QUERY =========================================== ~//
+	///
 	$rows = QueryDB($sql);
-
-	/*$rows = array();
-	$rows[-1] = array('SSN', 'First_Name', 'Last_Name', 'Gender');
-	$rows[0] = array(128629865, 'Sherlock', 'Holmes', 'M');
-	$rows[1] = array(577974093, 'Moycroft', 'Holmes', 'M');
-	$rows[2] = array(965836254, 'John', 'Watson', 'M');
-	$rows[3] = array(971518441, 'Irene', 'Adler', 'F');
-	$rows[4] = array(816629811, 'James', 'Moriarty', 'M');
-	$rows[5] = array(164816494, 'Greggory', 'Lestate', 'M');*/
 
 	return GenerateTable($rows);
 }
